c2049e73f6baab4dc5a5a9ee6d61f5daa492affa
matteo00gm <matteo00gm@gmail.com>
2025-05-10 12:03:24 +0200

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const isGithubPages = process.env.NODE_ENV === 'production' && process.env.DEPLOY_TARGET === 'github_pages';
+
+const nextConfig = {
+  output: isGithubPages ? 'export' : undefined,
+  distDir: isGithubPages ? 'dist' : '.next', // Optional: specify a different output directory
+  basePath: isGithubPages ? '/CV_Matteo_Grosso' : undefined, // Replace '/CV_Matteo_Grosso' with your repository name
+  assetPrefix: isGithubPages ? '/CV_Matteo_Grosso/' : undefined, // Replace '/CV_Matteo_Grosso/' with your repository name and a trailing slash
+};
+
+module.exports = nextConfig;
